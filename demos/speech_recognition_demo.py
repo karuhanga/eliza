e76ae2993853f8d24fec38d223cc7b432e0edb88
karuhanga <karuhanga475@gmail.com>
2018-12-16 13:35:21 +0300

    Added kaldi speech recognition option

--- demos/speech_recognition_demo.py
+++ demos/speech_recognition_demo.py
@@ -1,7 +1,9 @@
 import speech_recognition as sr
 
+from demos.custom_recognizers import RecognizerWithKaldi
+
+r = RecognizerWithKaldi()
 # obtain audio from the microphone
-r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
@@ -29,5 +31,17 @@ def use_pocket_sphinx():
         print("Sphinx error; {0}".format(e))
 
 
+def use_kaldi():
+    # recognize speech using kaldi-gstreamer-server at localhost
+    kaldi_host = "localhost:8888"
+    try:
+        print("Kaldi thinks you said " + str(r.recognize_kaldi(audio, host=kaldi_host)))
+    except sr.UnknownValueError:
+        print("Kaldi could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Kaldi; {0}".format(e))
+
+
 use_google_cloud_speech_api()
 use_pocket_sphinx()
+use_kaldi()
