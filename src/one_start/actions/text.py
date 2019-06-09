ed037aa2b9dbed65056b88a87a9f627342ebb019
Lincoln Karuhanga <karuhanga475@gmail.com>
2019-06-09 14:13:07 +0300

    - Add weather

--- src/one_start/actions/text.py
+++ src/one_start/actions/text.py
@@ -1,3 +1,4 @@
+from src.one_start.actions.weather import run
 from src.utils.constants import ACTIONS, LAUNCHER_COMMANDS, \
     get_launcher_commands
 
@@ -64,6 +65,10 @@ def what_can_you_do_action():
         """
 
 
+def weather_action():
+    return run()
+
+
 def build_action(name, steps, last_n_prompts, action):
     return {
         'name': name,
@@ -79,6 +84,7 @@ def build_actions():
         'search': build_action("search", 2, ["What would you like me to search for?", "Searching "], search_action),
         'time': build_action("time", 1, [""], time_action),
         'what can you do?': build_action("what can you do?", 1, [""], what_can_you_do_action),
+        'weather': build_action('weather', 1, ["It'll be "], weather_action)
     }
     quick_actions = [{action: build_action(action, 1, ["Done."], resolve_keyword_action(action))} for action in ACTIONS.keys()]
     for action in quick_actions:
---
+++ src/one_start/actions/weather.py
@@ -0,0 +1,68 @@
+import requests.exceptions as Error
+import requests
+
+API_KEY = "e642d8c438804deab5741155170507"
+CORE_URL = "http://api.apixu.com/v1/"
+PATH_FOR_CURRENT = CORE_URL + "current.json?key=" + API_KEY
+PATH_FOR_FORECAST = CORE_URL + "forecast.json?key=" + API_KEY
+data = {}
+
+
+# gets integer, prompting retry on invalid input
+def get_int(notif):
+    number = input(notif)
+    while (number.isdigit() == False):
+        number = input("Please input a valid choice:  ")
+    return int(number)
+
+
+# gets extra options for forecast request
+def get_forecast_options():
+    days = get_int("How many days in the future are we looking?(1~10): ")
+    while days > 10 or days < 1:
+        days = get_int("We can only look 1~10 days in the future :(: ")
+    return {"days": days}
+
+
+def run(where='Kampala', when=1, how_many=2):
+    city = where
+
+    data["q"] = city
+
+    pref = when
+
+    if pref is 2:
+        data.update({"days": how_many})
+
+    pref = PATH_FOR_FORECAST if pref is 2 else PATH_FOR_CURRENT
+
+    print("Getting weather forecast for " + city + "...")
+
+    try:
+        reply = requests.get(pref, params=data)
+    except Error.ConnectionError as e:
+        return (
+            "The Service is currently down, please retry in a few seconds :)")
+
+    if not reply.status_code == requests.codes.ok:
+        return (
+            "The Service is currently down, please retry in a few seconds :)")
+
+    reply = (reply.json())["current"] if pref is PATH_FOR_CURRENT else \
+        (reply.json())["forecast"]["forecastday"]  # extract current object
+
+    if pref is PATH_FOR_CURRENT:
+        return (city + " is " + reply["condition"]["text"] + " today, at " + str(
+            reply["temp_c"]) + " degrees. " + "It feels like " + str(
+            reply["feelslike_c"]) + " degress though!")
+    else:
+        message = ''
+        for day in range(0, data["days"]):
+            message = message + ("Tomorrow, " if day is 0 else "Then the next day, ")
+            message = message + city + " will be " + reply[day]["day"]["condition"][
+                "text"] + ", at " + str(
+                reply[day]["day"]["avgtemp_c"]) + " degrees on average. "
+        return message
+
+if __name__ == "__main__":
+    print(run())
